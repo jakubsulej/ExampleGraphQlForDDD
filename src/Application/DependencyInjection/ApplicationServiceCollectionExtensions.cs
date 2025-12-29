dfeaaadbcabe7d19169623462700439fcef006da
Jakub Sulej <jakub.sulej@oroox.com>
2025-12-29 17:44:22 +0100

    registering mediator

--- src/Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
+++ src/Application/DependencyInjection/ApplicationServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Application.DependencyInjection;
 
@@ -6,6 +7,13 @@ public static class ApplicationServiceCollectionExtensions
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        var assembly = typeof(ApplicationServiceCollectionExtensions).Assembly;
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
         return services;
     }
 }
--- src/README.md
+++ src/README.md
@@ -1,7 +1,20 @@
+#Ef migration
+
 ```bash
 	// add new migration
 	Add-Migration <YourMigrationName> -Project Infrastructure -StartupProject WebApi -Context Infrastructure.EntityFramework.ServiceDbContext -OutputDir EntityFramework\Migrations
 	
 	// update database
 	Update-Database -Project Infrastructure -StartupProject WebApi -Context Infrastructure.EntityFramework.ServiceDbContext
+```
+
+#Example query
+```
+	query GetServiceOffersPage($page: Int!, $pageSize: Int!) {
+	  serviceOffersPage(page: $page, pageSize: $pageSize) {
+		serviceOffers {
+		  id
+		}
+	  }
+	}
 ```
